378a0929a7f8d68e3bc5e909f76d198340c6b7b6
Raflan19 <raflan835@gmail.com>
2025-02-28 00:31:24 +0530

    deleted demo files

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const req=document.getElementById("log");
-const div1=document.getElementById("logsignup");
-req.addEventListener("click",(ev)=>{
-    div1.innerHTML="";
-    let div2=document.createElement("button");
-    div2.textContent="😎User1";
-    div1.append(div2);
-})
